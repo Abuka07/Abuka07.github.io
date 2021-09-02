418c3faa5e1fa2c3abb219f454dd91467822944a
Abuka07 <kvantoviu11@gmail.com>
2021-09-02 20:20:37 +0600

    my first commit

---
+++ project8/style/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: true,
+        autoplay:true,
+        autoplaytimeout: 2000   ,
+        items:2,
+    })
+  });
